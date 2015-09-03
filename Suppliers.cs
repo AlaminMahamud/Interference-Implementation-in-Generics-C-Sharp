7fac652f6fb6f8582c809de0f29d005af28caa6b
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2015-09-03 13:42:43 +0600

    Include the using statement to ensure that IPhoneNumber is Accessible

--- Suppliers.cs
+++ Suppliers.cs
@@ -1,4 +1,7 @@
 // A Class of phone numbers for suppliers
+
+using n1;
+
 class Supplier(string n, string num)
 {
 	public Supplier(string n, string num)
@@ -13,4 +16,5 @@ class Supplier(string n, string num)
 
 	//..
 }
+
 
